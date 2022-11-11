30ff50ee9bd4a1326fc719cd7bb75da3e7feb421
allen dean <allen7242@gmail.com>
2022-11-11 12:26:19 -0600

    Modified statejs

--- motivquotestate.js
+++ motivquotestate.js
@@ -1,180 +1,93 @@
 // Import our database
-import { database } from './database';
+import Database from "./database.js";
 
-// Lines in questions-
 // Create a new state manager
-class StoreManager {
-  constructor(init = {}) {
-    // Grab the current "this" context
-    const self = this;
-    // Create an array for subscribers
-    this._subscribers = [];
+export default class StateManager {
+  constructor() {
+    // initialize the data store.
+    // This is our state. When anything changes
+    // with any of these variables, we need to
+    // notify out components:
+    this.quotes = [];
+    this.searchResults = [];
+    this.favorites = [];
+    this.subscribers = [];
+    this.searchMode = true;
+    this.showNotes = false;
+    this.database = new Database();
+    this.loadFavorites();
 
-    // // Wait for our database to be ready
-    database.then(async (db) => {
-      // When ready, make the database available to the state manager
-      this.db = db;
+    // Listening so that any time a "like-requested" event happens, it
+    // will call the "saveQuoteToFavorites" method.
 
-      // Get the key from the database
-      const key = await db.get('settings', 'key');
-      if (key) {
-        this._state.key = key;
-      }
-
-      // Get favorites from the database
-      const favorites = await db.get('settings', 'favorites');
-      if (favorites) {
-        this._state.favorites = favorites;
-      }
-
-      // Get notes from the database
-      const notes = await db.getAll('notes');
-      if (notes.length) {
-        this._state.notes =
-          Object.fromEntries(notes.map(({ imdbID, note }) => [imdbID, note])) ||
-          {};
-      }
-    });
-
-    // Create the internal state proxy
-    this._state = new Proxy(init, {
-      // Make set an async function so we can use our database to save it when it's updated
-      async set(target, key, value) {
-        // Set the key equal to the value
-        target[key] = value;
-
-        // Save items to the database, if it's available
-        if (self.db) {
-          if (key === 'key') {
-            await self.db.put('settings', value, 'key');
-          }
-
-          if (key === 'favorites') {
-            await self.db.put('settings', value, 'favorites');
-            const quotes = target.quotes
-              .filter((quote) => value.includes(quote.imdbID))
-              .map((quote) => self.db.put('quotes', quote));
-
-            await Promise.all(quotes);
-          }
-
-          if (key === 'notes') {
-            await Promise.all(
-              Object.entries(value).map(([id, note]) =>
-                self.db.put('notes', { note, imdbID: id }),
-              ),
-            );
-          }
-        }
-
-        // Loop through each subscriber and call them
-        for (const subscriber of self._subscribers) {
-          subscriber(target);
-        }
-
-        // Return true to indicate that the set was successful
-        return true;
-      },
-    });
+    this.subscribe("like-requested", this.saveQuoteToFavorites.bind(this));
+    this.subscribe("quote-found", this.setSearchResults.bind(this));
+    this.subscribe("favorites-loaded", this.setFavorites.bind(this));
+    this.subscribe("show-notes", this.toggleNotes.bind(this));
+    this.subscribe("save-requested", this.saveQuoteToFavorites.bind(this));
   }
 
-  // Let things subscribe to the state manager
-  subscribe(cb) {
-    if (typeof cb !== 'function') {
-      throw new Error('Callback is not a function');
-    }
-    this._subscribers.push(cb);
-    cb(this._state);
+  setSearchResults(quoteDataList) {
+    this.searchResults = quoteDataList;
+    this.quotes = this.searchResults;
   }
 
-  // Get the current state
-  state(key) {
-    if (key) {
-      return this._state[key];
-    }
-    return this._state;
+  setFavorites(quoteDataList) {
+    this.favorites = quoteDataList;
+    this.quotes = this.favorites;
   }
 
-  // Action to add a comment to the state
-  search(lookup) {
-    if (lookup.key) {
-      this._state.key = lookup.key;
-    }
+  toggleNotes(val) {
+    this.showNotes = val;
+    this.notify("redraw", this.quotes);
+  }
 
-    this._state.search = {
-      title: lookup.title,
-      year: lookup.year || '',
+  // A method to read a user's favorites from IndexedDB  when the page first loads.
+  loadFavorites() {
+    // reads from IndexDB and stores the
+    // data to "this.favorites." Then,
+    // notifies any interested componenets.
+    const callbackFunction = function (quoteDataList) {
+      this.notify("favorites-loaded", quoteDataList);
     };
-
-    this._state.error = '';
-
-    this.quoteLookup();
+    // invoke the getAll function
+    // going to load all the records and then notify everyone
+    this.database.getAll(callbackFunction.bind(this));
   }
 
-  reset() {
-    this._state.search = null;
-    this._state.quotes = [];
+  // A method to add a new movie to the user's
+  // favorites and save it to IndexedDB.
+  saveMovieToFavorites(quoteData) {
+    // appends the movie to this.favorites and
+    // stores it in the DB.
+    console.log("I am about to save the quote to the DB!");
+    console.log(quoteData);
+    this.database.addOrUpdate(quoteData, function () {
+      console.log("Successfully added to the database");
+    });
   }
 
-  toggleNoteVisibility(id) {
-    const visibility = this.state('visibleNotes') || {};
-    if (visibility[id] === true) {
-      visibility[id] = false;
-    } else {
-      visibility[id] = true;
-    }
-    this._state.visibleNotes = visibility;
-  }
+  // A method to notify components that something has changed.
+  notify(eventName, data) {
+    // loops through all of the subscribers and invokes the subscribers's function
+    // if they're interested in the particular event
+    for (let i = 0; i < this.subscribers.length; i++) {
+      const subscriber = this.subscribers[i];
 
-  toggleFavorite(id) {
-    const favorites = this.state('favorites') || [];
+      const subscriberEvent = subscriber[0];
+      const callbackFunction = subscriber[1];
+      // is event that was just fired something
 
-    if (favorites.includes(id)) {
-      favorites.splice(favorites.indexOf(id), 1);
-    } else {
-      favorites.push(id);
-    }
-
-    this._state.favorites = favorites;
-  }
-
-  setNote(id, note) {
-    const notes = this.state('notes') || {};
-    notes[id] = note;
-    this._state.notes = notes;
-  }
-
-  async quoteLookup() {
-    let key = this.state('key');
-    let search = this.state('search');
-
-    if (!key) {
-      return (this._state.error = 'No API Key');
-    }
-
-    let query = `https://www.omdbapi.com/?apikey=${key}&s=${search.title}`;
-    if (search.year) {
-      query += `&y=${search.year}`;
-    }
-
-    try {
-      const response = await fetch(query);
-      const data = await response.json();
-      if (data.Response === 'False') {
-        // Show error message
-        this._state.error = data.Error;
+      if (eventName == subscriberEvent) {
+        callbackFunction(data);
       }
-
-      // Make sure this is an array
-      if (Array.isArray(data.Search)) {
-        this._state.quotes = data.Search;
-      } else {
-        this._state.quotes = [];
-      }
-    } catch (e) {
-      this._state.error = e.message;
     }
+  }
+
+  subscribe(eventName, callbackFunction) {
+    // when a components wants to scbsxribe to the stateManager,
+    // they need to tell the sataeManager which event they're interested in,
+    // and what should happen if that event is fired (callback function).
+    this.subscribers.push([eventName, callbackFunction]);
   }
 }
-
-export const store = new StoreManager({ search: '', quotes: [] });
