6063156175b8dbd095a5fb33aaa9a048e7ac7f13
Kiana <104266214+kianazhane@users.noreply.github.com>
2022-11-17 22:01:06 -0600

    added new api & edited motivquotes html

--- motivquotes.js
+++ motivquotes.js
@@ -1,12 +1,24 @@
+const url = 'https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731';
+
+
+const menu = document.querySelector('header');
+const header = document.querySelector('main-nav');
+
+//document.getElementById("motivquotes").addEventListener("click", displayDate);
+
+//menu.addEventListener('click', function() {
+  
+//});
+
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'ad22b1bf14msh0f3ad872fa58e36p1bdad4jsn97ae05b8518e',
-		'X-RapidAPI-Host': 'healthruwords.p.rapidapi.com'
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
 	}
 };
 
-fetch('https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731', options)
+fetch('https://famous-quotes4.p.rapidapi.com/random?category=all&count=1', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
